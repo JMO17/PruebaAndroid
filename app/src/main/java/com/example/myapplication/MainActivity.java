2a92684c3a328e64639c5ad186ba85c06a52c010
jorge <21746516@live.uem.es>
2019-01-18 12:45:52 +0100

    initial2r

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     String hola;
+    String test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
